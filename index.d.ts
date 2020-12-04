a0400a700e6502bd69fb98f1657782695c252e6c
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 12:52:41 +0100

    1.2.0

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Library } from "@wowts/tslib";
-export interface AceComm {
-    RegisterComm(msgPrefix: string, method?: string | Callback): void;
-    SendCommMessage(msgPrefix: string, message: string, channel: string): void;
-}
-declare type Callback = (prefix: string, message: string, channel: string, sender: string) => void;
-declare const lib: Library<AceComm>;
-export default lib;
